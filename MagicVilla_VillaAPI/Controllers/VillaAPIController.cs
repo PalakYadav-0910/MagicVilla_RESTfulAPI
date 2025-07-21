9e2037fe88784aea1c887517510b235141da0121
PalakYadav-0910 <sy8220718@gmail.com>
2025-07-21 20:02:11 +0530

    Custom Logging Using Custom Class Logging

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Logging;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +10,13 @@ namespace MagicVilla_VillaAPI.Controllers
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
-        private readonly ILogger<VillaAPIController> _logger;
+        //Default Logging
+        //private readonly ILogger<VillaAPIController> _logger;
 
-        public VillaAPIController(ILogger<VillaAPIController> logger)
+        //Custom Logger
+        private readonly ILogging _logger;
+
+        public VillaAPIController(ILogging logger)
         {
             _logger = logger;
         }
@@ -20,7 +25,7 @@ namespace MagicVilla_VillaAPI.Controllers
         [ProducesResponseType(200, Type = typeof(VillaDTO))]
         public ActionResult GetVillas()
         {
-            _logger.LogInformation("Getting all Villas");
+            _logger.Log("Getting all Villas", "");
             return Ok(VillaStore.villaList);
         }
 
@@ -33,7 +38,7 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             if(id == 0)
             {
-                _logger.LogError("Get Villa Error with Id: " + id);
+                _logger.Log("Get Villa Error with Id: " + id, "error");
                 return BadRequest();
             }
             
